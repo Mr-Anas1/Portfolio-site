35360571d6ff36f4e3b23bc17835247e4993aa9c
Mohamed Anas <a3689211@gmail.com>
2024-10-04 20:23:41 +0530

    Some changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Hero.css'
+import main from '../../Assets/main.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="Hero">
+      <div className="hero-left">
+        <div className="hero-txt-container">
+          <div className="hero-txt">Hello,I'm </div>
+          <div className="hero-txt"> <span className='color-txt'>Mohamed Anas</span></div>
+          <div className="hero-txt">Frontend Developer</div>
+        </div>
+          
+      </div>
+
+      <div className="hero-right">
+        <div className="hero-right-img">
+          <img src={main} alt="img" />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import './Navbar.css'
+import { RiMenu2Line } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+    const [closeIcon, setCloseIcon] = useState(1);
+
+    const handleClick = () => {
+        setCloseIcon(closeIcon === 1? 0 : 1);
+    }
+
+    return (
+        <div className="Navbar">
+            <div className="navbar-container">
+                <div className="navbar-left">
+                    <div className="navbar-name">Mohamed Anas</div>
+                </div>
+
+                <div className={`navbar-right ${closeIcon === 0 ? 'navbar-right-mobile' : ''}`}>
+                {console.log(closeIcon)}
+                    <div className={`navbar-links ${closeIcon === 0 ? 'navbar-links-mobile' : ''}`}>
+                        <li>About</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </div>
+                    
+                </div>
+                {
+                        (closeIcon) ?
+                            <RiMenu2Line className="navbar-menu-icon hidden" onClick={handleClick}/>
+                        :
+                            <AiOutlineClose className='navbar-menu-icon hidden' onClick={handleClick} />
+                        
+
+                    }
+            </div>
+        
+            <div className="navbar-line"></div>
+
+        </div>
+    )
 }
 
 export default Navbar
