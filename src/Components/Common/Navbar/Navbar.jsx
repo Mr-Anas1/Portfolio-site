7f57fa1757df5c092f2f85a77c3dfaa20078062e
Mohamed Anas <a3689211@gmail.com>
2024-10-21 19:44:06 +0530

    Bugs fixed

--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Navbar.css'
 import { RiMenu2Line } from "react-icons/ri";
 import { AiOutlineClose } from "react-icons/ai";
@@ -7,10 +7,27 @@ import { useState } from 'react';
 const Navbar = () => {
 
     const [closeIcon, setCloseIcon] = useState(1);
+    const [clickChange, setClickChange] = useState(0);
 
     const handleClick = () => {
         setCloseIcon(closeIcon === 1? 0 : 1);
     }
+
+    const handleClickChange = (value) => {
+        setClickChange(value !== undefined ? value : clickChange === 1 ? 0 : 1);
+    };
+
+    useEffect(()=>{
+        if(clickChange === 1){
+            document.documentElement.style.overflow = "hidden";
+        } else {
+            document.documentElement.style.overflow = '';
+        }
+
+        return () => {
+            document.documentElement.style.overflow = '';
+        }
+    },[clickChange])
 
     return (
         <div className="Navbar">
@@ -20,26 +37,24 @@ const Navbar = () => {
                 </div>
 
                 <div className={`navbar-right ${closeIcon === 0 ? 'navbar-right-mobile' : ''}`}>
-                {console.log(closeIcon)}
                     <div className={`navbar-links ${closeIcon === 0 ? 'navbar-links-mobile' : ''}`}>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#works">Projects</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#about" onClick={()=> {handleClickChange(0); handleClick();}}>About</a></li>
+                        <li><a href="#works"  onClick={()=> {handleClickChange(0); handleClick();}}>Projects</a></li>
+                        <li><a href="#contact"  onClick={()=> {handleClickChange(0); handleClick();}}>Contact</a></li>
                     </div>
-                    
                 </div>
                 {
                         (closeIcon) ?
-                            <RiMenu2Line className="navbar-menu-icon hidden" onClick={handleClick}/>
+                            <RiMenu2Line className="navbar-menu-icon hidden" onClick={() => {handleClick(); handleClickChange()}}/>
                         :
-                            <AiOutlineClose className='navbar-menu-icon hidden' onClick={handleClick} />
+                            <AiOutlineClose className='navbar-menu-icon hidden' onClick={() => {handleClick(); handleClickChange()}} />
                         
 
                     }
             </div>
         
             <div className="navbar-line"></div>
-
+            
         </div>
 
     )
